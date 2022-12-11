2ea92c19f80770db1af76a4297c88e0fe6678ae9
Zobair Hussain <49007316+ZobairRUET@users.noreply.github.com>
2022-12-11 11:15:19 +0600

    Add project files.

---
+++ TodoApis/Middlewares/GlobalExceptionHandlingMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace TodoApis.Middlewares
+{
+    public class GlobalExceptionHandlingMiddleware : IMiddleware
+    {
+		private readonly ILogger<GlobalExceptionHandlingMiddleware> _logger;
+
+        public GlobalExceptionHandlingMiddleware(ILogger<GlobalExceptionHandlingMiddleware> logger) => _logger = logger;
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+			try
+			{
+				await next(context);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError(e, e.Message);
+				context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+				ProblemDetails problem = new()
+				{
+					Type = "Error",
+					Title = "Error",
+					Status = (int)HttpStatusCode.InternalServerError,
+					Detail = "Internal server error occured"
+				};
+
+				var json = JsonConvert.SerializeObject(problem);
+
+				await context.Response.WriteAsync(json);
+				context.Response.ContentType = "application/json";
+
+			}
+        }
+    }
+}
---
+++ TodoApis/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApis.Middlewares;
+using TodoApis.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddLogging();
+builder.Services.AddTransient<GlobalExceptionHandlingMiddleware>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseMiddleware<GlobalExceptionHandlingMiddleware>();
+
+app.MapControllers();
+
+app.Run();
